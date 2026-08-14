18ad58a880fd4adbfeefd1065742dd4211ad24c0
Daniel Araújo <dannielaraujooficial@gmail.com>
2026-08-14 17:43:29 -0300

    fix: autorização realizada

--- boogly-api/src/app.js
+++ boogly-api/src/app.js
@@ -7,7 +7,14 @@ import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 
-app.use(cors());
+app.use(cors(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://seu-frontend.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+})));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
